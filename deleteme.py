9d51400d2da8056a7033f3965db73f5fa7155861
TrumanKen3 <tru2k@hotmail.com>
2022-09-27 14:04:48 +0000

    learninig about dictionaries

---
+++ deleteme.py
@@ -0,0 +1,8 @@
+installed to the */bin* directory. When this script is run,
+# bash will execute this script with *rm*, which will remove (delete)
+# the script :p#!/bin/rmcd mycode
+# This script calls on the executable called **rm** (remove), which is
+cd mycode# installed to the */bin* directory. When this script is run,
+# bash will execute this script with *rm*, which will remove (delete)
+# the script :p
+chmod u+x deleteme.pychmod u+x deleteme.pyls deleteme.py
